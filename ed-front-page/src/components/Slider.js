59b5a51369da330f2ed628904c3e3cd22ff1df32
tobix312 <stu21305@ashs.school.nz>
2023-08-15 14:51:47 +1200

    Frontpage V2
    We fixed the glitch, so now we only have one css class instead of two

--- ed-front-page/src/components/Slider.js
+++ ed-front-page/src/components/Slider.js
@@ -6,16 +6,15 @@ import { useInView } from "react-intersection-observer";
 
 const Slider = ({imageScr, title, subTitle, flipped}) => {
     
-    const {ref, inView, entry} = useInView({
-        threshold: 0.01,
+    const {ref, inView } = useInView({
+        threshold: 0.4,
     });
 
-    console.log (flipped)
     const renderContent = () => {
         if (!flipped) {
             return (
             <>
-                <img src={imageScr} alt="Meeting" className="slider_image"/>
+                <img src={imageScr} alt="Study" className="slider_image"/>
                 <div className = "slider_content">
                     <h1 className="slider_title">{title}</h1>
                     <p>{subTitle}</p>
@@ -25,15 +24,17 @@ const Slider = ({imageScr, title, subTitle, flipped}) => {
         }
         else {
             return (
-                <div className="sliders">
-                    <img src={imageScr} alt="Meeting" className="sliders_image"/>
-                    <div className = "sliders_content">
-                        <h1 className="sliders_title">{title}</h1>
+                <>
+                <div className="slider">
+                    <img src={imageScr} alt="Meeting" className="slider_image"/>
+                    <div className = "slider_content">
+                        <h1 className="slider_title">{title}</h1>
                         <p>{subTitle}</p>
                     </div>
                 </div>
-           )
-        };
+                </>
+           );
+        }
     };
     return <div className={inView ? "slider slider_zoom" : "slider"}ref={ref}>
         {renderContent()}</div>;
